31065239d10d6e4c7ffe66891b848aeb7161ff13
munsif <munsifmusthafa03@gmail.com>
2018-03-30 10:51:45 +0530

    Handled screen orientation changes

---
+++ app/src/main/java/com/emc/recipester/data/RecipeDataFragment.java
@@ -0,0 +1,30 @@
+package com.emc.recipester.data;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+/**
+ * Created by Munsif on 3/29/2018.
+ */
+
+public class RecipeDataFragment extends Fragment {
+    // data object we want to retain
+    private Recipe data;
+
+    // this method is only called once for this fragment
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // retain this fragment
+        setRetainInstance(true);
+    }
+
+    public void setData(Recipe data) {
+        this.data = data;
+    }
+
+    public Recipe getData() {
+        return data;
+    }
+}
--- app/src/main/java/com/emc/recipester/ui/RecipesListActivity.java
+++ app/src/main/java/com/emc/recipester/ui/RecipesListActivity.java
@@ -2,6 +2,7 @@ package com.emc.recipester.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,6 +16,7 @@ import com.emc.recipester.data.RecipesListAdapter;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.Serializable;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +26,8 @@ public class RecipesListActivity extends AppCompatActivity implements AdapterVie
     private String titleName = null;
     private RecipesListAdapter recipeListAdapter;
     List<Recipe> output = new ArrayList<>();
+    ListView listView;
+    static final String STATE_RECIPES = "recipesList.state";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,23 +38,27 @@ public class RecipesListActivity extends AppCompatActivity implements AdapterVie
             titleName = getIntent().getExtras().getString("category");
             getSupportActionBar().setTitle(titleName);
         } catch (NullPointerException e) {
-
         }
+
         RecipeService recipeService = new RecipeService(this);
         recipeService.requestRecipesByCategories(titleName);
 
-        ListView listView = findViewById(R.id.recipeListView);
-        listView.setOnItemClickListener(this);
+        if (savedInstanceState != null) {
+            listView = (ListView) savedInstanceState.getSerializable(STATE_RECIPES);
+        }
 
+        listView = findViewById(R.id.recipeListView);
+        listView.setOnItemClickListener(this);
         recipeListAdapter = new RecipesListAdapter(this, output);
         listView.setAdapter(recipeListAdapter);
+
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Intent intent = new Intent(this, DescriptionActivity.class);
         intent.putExtra("category", titleName);
-        intent.putExtra("recipeId", position + 1 );
+        intent.putExtra("recipeId", position + 1);
         RecipesListActivity.this.startActivity(intent);
     }
 
@@ -63,5 +71,11 @@ public class RecipesListActivity extends AppCompatActivity implements AdapterVie
         recipeListAdapter.clear();
         recipeListAdapter.addAll(output);
         recipeListAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putSerializable(STATE_RECIPES, (Serializable) listView.onSaveInstanceState());
     }
 }
